568c2f29a924e57a706b6260ce2d9e13c460b9f0
Usman Mehar <usmanghafoor571@gmail.com>
2024-02-20 14:25:38 +0500

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/traversy-media-vuejs/',
+  base: '/traveresy-media-vuejs/',
   plugins: [vue()],
 })
